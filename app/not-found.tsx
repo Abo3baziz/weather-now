2326feac9f57e5e6726bfdbce68263efe8885825
Ahmed Aziz <azez55.ali@gmail.com>
2026-04-06 03:03:04 +0200

    feat: add NotFound component for all un-matched routes

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import NotFound from "../components/Not-Found/Not-Found";
+
+export default function notFound() {
+  return <NotFound errorMessage="" errorTitle="" />;
+}
---
+++ components/Not-Found/Not-Found.tsx
@@ -0,0 +1,30 @@
+import styles from "./Not-Found.module.css";
+import MainWrapper from "../MainWrapper/MainWrapper";
+import iconError from "@/public/images/icon-error.svg";
+import iconRetry from "@/public/images/icon-retry.svg";
+
+import Image from "next/image";
+import Nav from "../Nav/Nav";
+import Header from "../Header/Header";
+
+export default function NotFound({
+  errorMessage = "We couldn't connect to the server (API error). Please try again in a few moments",
+  errorTitle = "Something went wrong",
+}: {
+  errorMessage: string | null;
+  errorTitle: string | null;
+}) {
+  return (
+    <MainWrapper>
+      <Nav />
+      <div className={styles.box}>
+        <Image src={iconError} alt="Error icon" width={48} />
+        <Header text={errorTitle} />
+        <p>{errorMessage}</p>
+        <button>
+          <Image src={iconRetry} alt="retry icon"></Image>Retry
+        </button>
+      </div>
+    </MainWrapper>
+  );
+}
